40c06d9a2c7c9d31989406bfc8e95d2addadfc36
Teslim Akande <teslimakande@gmail.com>
2022-04-03 10:19:13 +0100

    Read hypernym file

--- src/directed_graphs/WordNet.java
+++ src/directed_graphs/WordNet.java
@@ -2,17 +2,30 @@ package directed_graphs;
 
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
 
 public class WordNet {
 
+    private ArrayList<Bag<String>> synSets;
+    private ArrayList<Bag<Integer>> hypernyms;
+
     // constructor takes the name of the two input files
-    public WordNet(String synSetsFileName, String hypernyms) {
-        ArrayList<Bag<String>> synSets = extractSynSetsFromFileInput(synSetsFileName);
+    public WordNet(String synSetsFileName, String hypernymFileName) {
+        synSets = extractSynSetsFromFileInput(synSetsFileName);
+        hypernyms = extractHypernymsFromInputFile(hypernymFileName);
+
+        int counter = 0;
+        for (Bag<Integer> b : hypernyms) {
+            StdOut.print(counter);
+            for (Integer i : b) StdOut.print(", " + i);
+            StdOut.println();
+            ++counter;
+        }
     }
 
-    private int pointOfSynSetExtraction(String line) {
+    private int pointOfExtraction(String line) {
         int counter = 0;
         char c = line.charAt(counter);
         while (c != ',') {
@@ -22,14 +35,13 @@ public class WordNet {
         return counter + 1;
     }
 
-    private Bag<String> getBagOfStrings(int index, String line) {
+    private Bag<String> getBagOfSynonyms(int index, String line) {
         char c = line.charAt(index);
         StringBuilder word = new StringBuilder();
         Bag<String> wordBag = new Bag<>();
         while (c != ',') {
-            if (c != ' ') {
-                word.append(c);
-            } else {
+            if (c != ' ') word.append(c);
+            else {
                 wordBag.add(word.toString());
                 word = new StringBuilder();
             }
@@ -38,6 +50,24 @@ public class WordNet {
         }
         if (!word.isEmpty()) wordBag.add(word.toString());
         return wordBag;
+    }
+
+    private Bag<Integer> getBagOfSynSetReferences(int index, String line) {
+        char c = line.charAt(index);
+        Bag<Integer> synSets = new Bag<>();
+
+        StringBuilder referenceToSynSet = new StringBuilder();
+        while (true) {
+            if (c != ',') referenceToSynSet.append(c);
+            else {
+                synSets.add(Integer.parseInt(referenceToSynSet.toString()));
+                referenceToSynSet = new StringBuilder();
+            }
+            if (++index == line.length()) break;
+            c = line.charAt(index);
+        }
+        if (!referenceToSynSet.isEmpty()) synSets.add(Integer.parseInt(referenceToSynSet.toString()));
+        return synSets;
     }
 
     private ArrayList<Bag<String>> extractSynSetsFromFileInput(String fileName) {
@@ -50,12 +80,34 @@ public class WordNet {
 
         while (synSetFile.hasNextLine()) {
             line = synSetFile.readLine();
-            indexOfExtraction = pointOfSynSetExtraction(line);
-            words = getBagOfStrings(indexOfExtraction, line);
-            synSets.add(words);
+            indexOfExtraction = pointOfExtraction(line);
+            if (indexOfExtraction != -1) {
+                words = getBagOfSynonyms(indexOfExtraction, line);
+                synSets.add(words);
+            }
         }
 
         return synSets;
+    }
+
+    private ArrayList<Bag<Integer>> extractHypernymsFromInputFile(String fileName) {
+        In hypernymFile = new In(fileName);
+        ArrayList<Bag<Integer>> hypernyms = new ArrayList<>();
+
+        String line;
+        int indexOfExtraction;
+        Bag<Integer> synSetReferences;
+
+        while (hypernymFile.hasNextLine()) {
+            line = hypernymFile.readLine();
+            indexOfExtraction = pointOfExtraction(line);
+            if (indexOfExtraction != -1) {
+                synSetReferences = getBagOfSynSetReferences(indexOfExtraction, line);
+                hypernyms.add(synSetReferences);
+            }
+        }
+
+        return hypernyms;
     }
 
     // returns all WordNet nouns
@@ -76,7 +128,9 @@ public class WordNet {
         WordNet wn = new WordNet(
                 "C:\\Users\\ADMIN\\IdeaProjects\\Data Structures and Algorithms\\src\\directed_graphs" +
                         "\\synsets.txt",
-                null);
+                "C:\\Users\\ADMIN\\IdeaProjects\\Data Structures and Algorithms\\src" +
+                        "\\directed_graphs\\hypernyms.txt"
+        );
     }
 
 }
