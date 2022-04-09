3cb2818e05945aeb5b29cd154d5a92cd33570e46
Teslim Akande <teslimakande@gmail.com>
2022-04-09 22:31:28 +0100

    implemented length and ancestor methods

--- src/directed_graphs/SAP.java
+++ src/directed_graphs/SAP.java
@@ -2,6 +2,10 @@ package directed_graphs;
 
 import edu.princeton.cs.algs4.Digraph;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class SAP {
 
     private final Digraph diGraph;
@@ -16,20 +20,63 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        childCount = new int[diGraph.V()];
+        resetChildCount();
+        resetMarkedVertices();
         int ancestorPosition = dfs(v, w, true);
         if (ancestorPosition >= 0) return childCount[ancestorPosition];
         return ancestorPosition;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) { return dfs(v, w, true); }
+    public int ancestor(int v, int w) {
+        resetChildCount();
+        resetMarkedVertices();
+        return dfs(v, w, true);
+    }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w) { return 0; }
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        ArrayList<Integer> ancestors = new ArrayList<>(), lengths = new ArrayList<>();
+        int ancestor;
+        for (Integer i : v) {
+            for (Integer j : w) {
+                resetChildCount();
+                resetMarkedVertices();
+                ancestor = dfs(i, j, true);
+                if (ancestor >= 0) {
+                    if (Collections.binarySearch(ancestors, ancestor) < 0) {
+                        ancestors.add(ancestor);
+                        lengths.add(childCount[ancestor]);
+                    }
+                }
+            }
+        }
+        Collections.sort(lengths);
+        if (lengths.isEmpty()) return -1; else return lengths.get(0);
+    }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) { return 0; }
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        ArrayList<Integer> ancestors = new ArrayList<>(), lengths = new ArrayList<>();
+        HashMap<Integer, Integer> lengthHashMap = new HashMap<>();
+        int ancestor;
+        for (Integer i : v) {
+            for (Integer j : w) {
+                resetChildCount();
+                resetMarkedVertices();
+                ancestor = dfs(i, j, true);
+                if (ancestor >= 0) {
+                    if (Collections.binarySearch(ancestors, ancestor) < 0) {
+                        ancestors.add(ancestor);
+                        lengths.add(childCount[ancestor]);
+                        lengthHashMap.put(childCount[ancestor], ancestor);
+                    }
+                }
+            }
+        }
+        Collections.sort(lengths);
+        if (lengths.isEmpty()) return -1; else return lengthHashMap.get(lengths.get(0));
+    }
 
     private int dfs(int v, int w, boolean switchToV) {
         if (switchToV) {
